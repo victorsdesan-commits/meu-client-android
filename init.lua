6c1991f524cb9d30afb523601845f8117c00d6f8
victorsdesan-commits <victorsdesan@gmail.com>
2026-02-15 18:38:17 -0300

    Enviando arquivos do cliente 1511

--- init.lua
+++ init.lua
@@ -1,32 +1,39 @@
 -- CONFIG
-APP_NAME = "otclientv8"  -- important, change it, it's name for config dir and files in appdata
-APP_VERSION = 1341       -- client version for updater and login to identify outdated client
-DEFAULT_LAYOUT = "retro" -- on android it's forced to "mobile", check code bellow
+APP_NAME = "SinestroServer"  -- Nome da pasta de configuração
+APP_VERSION = 1511           -- Versão do cliente definida por você
+DEFAULT_LAYOUT = "retro"     -- No Android ele forçará o "mobile" automaticamente
 
--- If you don't use updater or other service, set it to updater = ""
+-- Removidos links externos para evitar lentidão ou erros no login
 Services = {
-  website = "https://docs.opentibiabr.com/",
-  updater = "",--"http://yourwebsite/api/updater.php"
-  stats = "", -- --"http://yourwebsite/api/stats.php"
-  crash = "", --"http://yourwebsite/api/crash.php",
-  feedback = "",--"http://yourwebsite/api/feedback.php",
-  status = "http://docs.opentibiabr.com" --http://yourwebsite/api/status.php"
+  website = "",
+  updater = "",
+  stats = "",
+  crash = "",
+  feedback = "",
+  status = ""
 }
 
--- Servers accept http login url, websocket login url or ip:port:version
-Servers = {
-  LocalTestServ = "127.0.0.1:7171:1100"
+-- Configuração direta do seu servidor
+WORLDS = {
+  {
+    worldid = 1,
+    worldname = 'Sinestro Server',
+    loginPort = 7171,
+    gameProxyPort = 7172,
+    proxies = {
+      "34.39.222.239", -- Seu IP de conexão
+    },
+  },
 }
 
---Server = "ws://yourwebsite:3000/"
---Server = "ws://127.0.0.1:88/"
---USE_NEW_ENERGAME = true -- uses entergamev2 based on websockets instead of entergame
-ALLOW_CUSTOM_SERVERS = true -- if true it shows option ANOTHER on server list
+Servers = {}
 
-g_app.setName("OTCv8")
+ALLOW_CUSTOM_SERVERS = true -- Permite usar o botão "Another" se necessário
+
+g_app.setName("Sinestro Server")
 -- CONFIG END
 
--- print first terminal message
+-- Logs de inicialização no terminal
 g_logger.info(os.date("== application started at %b %d %Y %X"))
 g_logger.info(g_app.getName() .. ' ' .. g_app.getVersion() .. ' (' .. g_app.getBuildCommit() .. ') made by ' .. g_app.getAuthor() .. ' built on ' .. g_app.getBuildDate() .. ' for arch ' .. g_app.getBuildArch())
 
@@ -38,10 +45,10 @@ if not g_resources.directoryExists("/modules") then
   g_logger.fatal("Modules dir doesn't exist.")
 end
 
--- settings
+-- Carregar configurações
 g_configs.loadSettings("/config.otml")
 
--- set layout
+-- Definir Layout (Mobile/Retro)
 local settings = g_configs.getSettings()
 local layout = DEFAULT_LAYOUT
 if g_app.isMobile() then
@@ -51,36 +58,26 @@ elseif settings:exists('layout') then
 end
 g_resources.setLayout(layout)
 
--- load mods
+-- Carregamento de Módulos
 g_modules.discoverModules()
 g_modules.ensureModuleLoaded("corelib")
 
 local function loadModules()
-  -- libraries modules 0-99
+  -- Módulos de biblioteca
   g_modules.autoLoadModules(99)
   g_modules.ensureModuleLoaded("gamelib")
 
-  -- client modules 100-499
+  -- Módulos do cliente
   g_modules.autoLoadModules(499)
   g_modules.ensureModuleLoaded("client")
 
-  -- game modules 500-999
+  -- Módulos de interface de jogo
   g_modules.autoLoadModules(999)
   g_modules.ensureModuleLoaded("game_interface")
 
-  -- mods 1000-9999
+  -- Outros mods
   g_modules.autoLoadModules(9999)
 end
 
--- report crash
-if type(Services.crash) == 'string' and Services.crash:len() > 4 and g_modules.getModule("crash_reporter") then
-  g_modules.ensureModuleLoaded("crash_reporter")
-end
-
--- run updater, must use data.zip
-if type(Services.updater) == 'string' and Services.updater:len() > 4
-  and g_resources.isLoadedFromArchive() and g_modules.getModule("updater") then
-  g_modules.ensureModuleLoaded("updater")
-  return Updater.init(loadModules)
-end
+-- Desativado reporte de erro e updater para focar na conexão direta
 loadModules()
